0ff0cfbe02c4c40b90b7892a886ebd8c334d1f1f
Abdullah Moubayed <moubayed.abdullah@icloud.com>
2022-12-08 13:23:26 +0100

    debug

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -200,17 +200,3 @@ exports.getMonthlyPlan = async (req, res) => {
     });
   }
 };
-
-exports.importMovies = async (req, res) => {
-  try {
-    res.status(200).json({
-      status: 'success',
-      message: 'Hej Iddis',
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
--- models/movieModel.js
+++ models/movieModel.js
@@ -2,126 +2,133 @@ const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
 
-const movieSchema = new mongoose.Schema({
-  adult: {
-    type: Boolean,
-    default: false,
-  },
-  backdrop_path: {
-    type: String,
-    default: null,
-  },
-  belongs_to_collection: {
-    type: Object,
-    default: null,
-  },
-  budget: {
-    type: Number,
-    required: [true, 'Du måste ange budget!'],
-  },
-  genres: {
-    type: Array,
-    required: [true, 'Du måste ange genre!'],
-  },
-  homepage: {
-    type: String,
-    default: null,
-  },
-  imdb_id: {
-    type: String,
-    default: null,
-    unique: [true, 'Filmen måste vara unik!'],
-    minLength: [9, 'Ange rätt id! Iden måste innehålla 9 karaktärer!'],
-    maxLength: [9, 'Ange rätt id! Iden måste innehålla 9 karaktärer!'],
-    validate: {
-      validator: function (val) {
-        return /^tt[0-9]{7}/.test(val);
+const movieSchema = new mongoose.Schema(
+  {
+    adult: {
+      type: Boolean,
+      default: false,
+    },
+    backdrop_path: {
+      type: String,
+      default: null,
+    },
+    belongs_to_collection: {
+      type: Object,
+      default: null,
+    },
+    budget: {
+      type: Number,
+      required: [true, 'Du måste ange budget!'],
+    },
+    genres: {
+      type: Array,
+      required: [true, 'Du måste ange genre!'],
+    },
+    homepage: {
+      type: String,
+      default: null,
+    },
+    imdb_id: {
+      type: String,
+      default: null,
+      unique: [true, 'Filmen måste vara unik!'],
+      minLength: [9, 'Ange rätt id! Iden måste innehålla 9 karaktärer!'],
+      maxLength: [9, 'Ange rätt id! Iden måste innehålla 9 karaktärer!'],
+      validate: {
+        validator: function (val) {
+          return /^tt[0-9]{7}/.test(val);
+        },
+        message: 'Ange rätt id!',
       },
-      message: 'Ange rätt id!',
     },
-  },
-  original_language: String,
-  original_title: {
-    type: String,
-    required: [true, 'Du måste ange rubriken!'],
-    unique: [true, 'Filmen måste vara unik!'],
-  },
-  overview: {
-    type: String,
-    required: [true, 'Du måste ange sammanfattning!'],
-  },
-  popularity: {
-    type: Number,
-    default: 0,
-  },
-  poster_path: {
-    type: String,
-    default: null,
-  },
-  production_companies: [
-    {
-      name: String,
-      id: Number,
-      logo_path: {
-        type: String,
-        default: null,
+    original_language: String,
+    original_title: {
+      type: String,
+      required: [true, 'Du måste ange rubriken!'],
+      unique: [true, 'Filmen måste vara unik!'],
+    },
+    overview: {
+      type: String,
+      required: [true, 'Du måste ange sammanfattning!'],
+    },
+    popularity: {
+      type: Number,
+      default: 0,
+    },
+    poster_path: {
+      type: String,
+      default: null,
+    },
+    production_companies: [
+      {
+        name: String,
+        id: Number,
+        logo_path: {
+          type: String,
+          default: null,
+        },
+        origin_country: String,
       },
-      origin_country: String,
+    ],
+    production_countries: [
+      {
+        iso_3166_1: String,
+        name: String,
+      },
+    ],
+    release_date: {
+      type: Date,
+      required: [true, 'Du måste ange utgivningsdatum!'],
     },
-  ],
-  production_countries: [
-    {
-      iso_3166_1: String,
-      name: String,
+    revenue: Number,
+    runtime: {
+      type: Number,
+      default: null,
     },
-  ],
-  release_date: {
-    type: Date,
-    required: [true, 'Du måste ange utgivningsdatum!'],
-  },
-  revenue: Number,
-  runtime: {
-    type: Number,
-    default: null,
-  },
-  spoken_languages: [
-    {
-      iso_639_1: String,
-      name: String,
+    spoken_languages: [
+      {
+        iso_639_1: String,
+        name: String,
+      },
+    ],
+    status: {
+      type: String,
+      enum: {
+        values: [
+          'Rumored',
+          'Planned',
+          'In Production',
+          'Post Production',
+          'Released',
+          'Canceled',
+        ],
+        message: 'Ange rätt läge!',
+      },
     },
-  ],
-  status: {
-    type: String,
-    enum: {
-      values: [
-        'Rumored',
-        'Planned',
-        'In Production',
-        'Post Production',
-        'Released',
-        'Canceled',
-      ],
-      message: 'Ange rätt läge!',
+    tagline: {
+      type: String,
+      default: null,
     },
+    title: String,
+    video: Boolean,
+    vote_average: {
+      type: Number,
+      default: 0,
+    },
+    vote_count: {
+      type: Number,
+      default: 0,
+    },
+    slug: String,
   },
-  tagline: {
-    type: String,
-    default: null,
-  },
-  title: String,
-  video: Boolean,
-  vote_average: {
-    type: Number,
-    default: 0,
-  },
-  vote_count: {
-    type: Number,
-    default: 0,
-  },
-});
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
-movieSchema.virtual('duration').get(function () {
-  return this.budget / 8;
+movieSchema.virtual('runtime-h').get(function () {
+  return `${Math.floor(this.runtime / 60)}h ${this.runtime % 60}min`;
 });
 
 // DOCUMENT MEDELWARE: Runs before .save() and .create()
